058ac8ccdda2fcf2289ef58cf264cd9190dcdb4b
Aanetra Vaidya <Aanetra.Vaidya@gmail.com>
2026-06-08 21:20:37 -0400

    Initial commit

---
+++ Kahlo/KahloApp.swift
@@ -0,0 +1,17 @@
+//
+//  KahloApp.swift
+//  Kahlo
+//
+//  Created by Aanetra Vaidya on 6/8/26.
+//
+
+import SwiftUI
+
+@main
+struct KahloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
